4affb332ade5b270cf1fb15fc4f206f1f8fe97aa
b9d9 <bnsneo33@gamil.com>
2026-01-16 11:27:12 +0900

    app.tsx 수정 - 강버들

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AppLayout } from "./layout/AppLayout";
-import { MainPage } from "./components/pages/MainPage";
-import { LoginPage } from "./components/pages/LoginPage";
+import { MainPage } from "./components/MainPage";
+import { LoginPage } from "./components/LoginPage";
 
 export default function App() {
   return (
